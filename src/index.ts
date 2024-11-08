55600c1e02166f352e5e24844c738b5c9e5f3225
Asif Islam <asif15-11288@diu.edu.bd>
2024-11-08 15:11:22 +0600

    Serving client in the root url from server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import express, { Request, Response, Router } from 'express';
-import { connectDatabase } from './config/database';
+import express from 'express';
+import cors  from 'cors';
+import path from 'path';
 import dotenv from 'dotenv';
+import { connectDatabase } from './config/database';
 import routes from './routes';
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 connectDatabase();
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/api', routes);
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
